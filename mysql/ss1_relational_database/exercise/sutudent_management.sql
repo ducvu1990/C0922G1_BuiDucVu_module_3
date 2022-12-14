554bf7b63bfd20abfbef6c13fbc67ae608709adb
Đức Vũ Bùi <cubinqtvn15@gmail.com>
2022-12-14 15:16:36 +0700

    up

---
+++ mysql/ss1_relational_database/exercise/sutudent_management.sql
@@ -0,0 +1,14 @@
+create database student_management;
+use student_management;
+
+create table class(
+id int,
+`name` varchar(50)
+);
+
+create table teacher(
+id int,
+`name` varchar(50),
+age int,
+country varchar(50)
+);
