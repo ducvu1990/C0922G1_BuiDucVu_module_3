3ca1740558fe18e49e3d5a0ae6e9f74973d3e27e
Đức Vũ Bùi <cubinqtvn15@gmail.com>
2022-12-19 13:35:52 +0700

    bai_tap

--- mysql/ss4_common_functions_in_SQL/exercise/practice_common_functions_in_SQL.sql
+++ mysql/ss4_common_functions_in_SQL/exercise/practice_common_functions_in_SQL.sql
@@ -1,19 +1,22 @@
-use quan_ly_sinh_vien;
+use
+quan_ly_sinh_vien;
 
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
-select * from subject
-where CREDIT like (select max(CREDIT) from subject);
+select *
+from subject
+where CREDIT in (select max(CREDIT) from subject);
 
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
 
-select s.* from subject s
-right join mark m on s.SUB_ID = m.SUB_ID
-where MARK like (select max(MARK) from mark);
+select s.*
+from subject s
+         join mark m on s.SUB_ID = m.SUB_ID
+where MARK in (select max(MARK) from mark);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 
-select st.*,avg(MARK) as avg_mark
-from  student st
-join mark m on st.STUDENT_ID = m.STUDENT_ID
+select st.*, avg(MARK) as avg_mark
+from student st
+         join mark m on st.STUDENT_ID = m.STUDENT_ID
 group by STUDENT_ID
-order by avg_mark ;
+order by avg_mark;
