febc0aeab54f830f474cda1d78b2a17c58111601
Đức Vũ Bùi <cubinqtvn15@gmail.com>
2022-12-29 21:24:37 +0700

    bai_tap

--- java_web/ss9_web_application_verview/exercise/product_discount_calculator_app/src/main/java/com/example/product_discount_calculator.java
+++ java_web/ss9_web_application_verview/exercise/product_discount_calculator_app/src/main/java/com/example/product_discount_calculator.java
@@ -4,8 +4,9 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "product_discount_calculator", value = "/product_discount_calculator")
+@WebServlet(name = "product_discount_calculator", value = "/display-discount")
 public class product_discount_calculator extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -14,6 +15,20 @@ public class product_discount_calculator extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String productDescription = request.getParameter("product_description");
+        float listPrice = Float.parseFloat(request.getParameter("list_price"));
+        float discountPercent = Float.parseFloat(request.getParameter("discount_percent"));
 
+        float discountAmount = listPrice * discountPercent *0.01f;
+        float discountPrice = listPrice - discountAmount;
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>" + productDescription + "</h1>");
+        writer.println("<h1>" + listPrice + "</h1>");
+        writer.println("<h1>" + discountPercent + "</h1>");
+        writer.println("<h1>" + discountAmount + "</h1>");
+        writer.println("<h1>" + discountPrice + "</h1>");
+        writer.println("</html>");
     }
 }
--- java_web/ss9_web_application_verview/exercise/product_discount_calculator_app/src/main/webapp/index.jsp
+++ java_web/ss9_web_application_verview/exercise/product_discount_calculator_app/src/main/webapp/index.jsp
@@ -6,13 +6,17 @@
 </head>
 <body>
 <h1>Product Discount Calculator</h1>
-<form action="">
-    <label></label><br>
-    <input type="text">
-    <label></label><br>
-    <input type="text">
-    <label></label><br>
-    <input type="text">
+<form action="/display-discount" method="post">
+    <label>Product Description: </label><br>
+    <input type="text" name="product_description" size="30"><br>
+
+    <label>List Price: </label><br>
+    <input type="text" name="list_price" size="30"><br>
+
+    <label>Discount Percent: </label><br>
+    <input type="text" name="discount_percent">
+
+    <input type="submit" value="Calculate Discount">
 </form>
 </body>
 </html>
